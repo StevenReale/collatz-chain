cc0eb44b5263a3662b2082278637c892217101fd
StevenReale <112089327+StevenReale@users.noreply.github.com>
2022-09-06 12:05:37 -0400

    Add files via upload

---
+++ collatz-v3.py
@@ -0,0 +1,37 @@
+#initializes variables
+chains = {1: 1} #this dictionary stores chain lengths for tested inputs
+longest_chain = 0
+longest_chain_item = 1
+testMax = 1000000
+
+#function is passed the number to test as well as the dictionary to test it on
+def collatz(start_num, chain_dict):
+    #if number to test is already in the dictionary, returns dictionary unchanged
+    if start_num in chain_dict:
+        return chain_dict
+
+    #otherwise, determines next number to test
+    elif start_num % 2 == 0:
+        nextCol = start_num/2
+    else:
+        nextCol = 3 * start_num  + 1
+
+    #passes next number to test into this function and checks returned dictionary for the chain length of next number on chain and adds 1 to it
+    newVal = collatz(nextCol, chain_dict)
+    chain_dict[start_num] = newVal[nextCol] + 1
+    return chain_dict
+
+#loop populates dictionary with keys from 1 to testMax
+for i in range(1, testMax, 1):
+    if i in chains:
+        continue
+    print("testing", i)
+    chains = collatz(i, chains)
+
+#loop reads dictionary to find longest chain and its associated key
+for item in chains:
+    if chains[item] > longest_chain:
+        longest_chain=chains[item]
+        longest_chain_item = item
+
+print("The longest chain starts with", longest_chain_item, "and has", longest_chain, "terms.")
